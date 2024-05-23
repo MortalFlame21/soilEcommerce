fee086e575c3c48e032d49ee6702a286814cc59f
Russell <russells0014@gmail.com>
2024-05-23 13:47:34 +1000

    refactor: Refactor createCart function to handle user ID parameter

--- server/src/controllers/cart.controller.js
+++ server/src/controllers/cart.controller.js
@@ -1,13 +1,37 @@
 const db = require("../database");
+const user = require("../database/models/user");
 
-const createCart = async () => {
-    return await db.cart.create();
+const createCart = async (id) => {
+    try {
+        if (id) {
+            return await db.cart.create({
+                user_id: id
+            });
+        };
+        return await db.cart.create();
+    } catch (error) {
+        console.error("Error in createCart:", error);
+
+    }
 };
+
 
 exports.createOrFindCart = async (req, res) => {
     try {
+        const { cart_id: cartId, user_id: userId } = req.body;
 
+        let cart = null;
+        if (cartId) {
+            cart = await db.cart.findByPk(cartId);
+        }
+        if (userId) {
+            cart = await db.cart.findOne({ where: { user_id: userId } });
+        }
+        if (cart === null) {
+            cart = await createCart(userId);
+        }
 
+        res.json(cart);
     } catch (error) {
         console.error("Error in createOrFindCart:", error);
         res.status(500).json({ error: 'Internal server error' });
