b86418145845dc4bce1c4f4d641bde5dc9cf79f4
2trill2code <s4007180@student.rmit.edu.au>
2024-05-29 22:05:43 +1000

    add service for review routes

---
+++ client/src/service/review.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+import config from "./config";
+
+export async function getProductReviews(id: number) {
+  try {
+    const res = await axios.get(`${config.HOST}/reviews/?product_id=${id}`);
+    return res.data;
+  } catch {
+    return [];
+  }
+}
+
+export async function createReview(
+  user_id: number,
+  product_id: number,
+  description: string,
+  stars: number
+) {
+  try {
+    const res = await axios.post(`${config.HOST}/reviews/`, {
+      user_id: user_id,
+      product_id: product_id,
+      description: description,
+      stars: stars,
+    });
+    return res.data;
+  } catch {
+    return [];
+  }
+}
+
+export async function updateReview(
+  user_id: number,
+  product_id: number,
+  description: string,
+  stars: number
+) {
+  try {
+    const res = await axios.patch(`${config.HOST}/reviews/`, {
+      user_id: user_id,
+      product_id: product_id,
+      description: description,
+      stars: stars,
+    });
+    return res.data;
+  } catch {
+    return [];
+  }
+}
+
+export async function deleteReview(user_id: number, product_id: number) {
+  try {
+    const res = await axios.delete(
+      `${config.HOST}/reviews/?user_id=${user_id}&product_id=${product_id}`
+    );
+    return res.data;
+  } catch {
+    return [];
+  }
+}
