88f5b18f9b76a8f89a7779f8be28547fdd3519e6
2trill2code <s4007180@student.rmit.edu.au>
2024-05-29 21:29:45 +1000

    add validation for post route

--- server/src/controllers/review.controller.js
+++ server/src/controllers/review.controller.js
@@ -1,6 +1,8 @@
 const db = require("../database");
+const { validateUserID } = require("../utils/users");
+const validate = require("validator");
 
-// todo: valite .create
+// todo: validate .create
 
 // Get all reviews for the single product
 exports.getSingleProduct = async (req, res) => {
@@ -23,18 +25,53 @@ exports.getSingleProduct = async (req, res) => {
 // Create a new review
 exports.create = async (req, res) => {
   const { user_id, product_id, description, stars } = req.body;
-
-  await db.review.create({
-    user_id: user_id,
-    product_id: product_id,
-    description: description,
-    stars: stars,
-  });
-
-  res.status(200).json({
-    message: `Success. user_id: ${user_id} has reviewed product_id: ${product_id}`,
-  });
   try {
+    const idInvalid = await validateUserID(user_id);
+    if (idInvalid) {
+      res.status(500).json({ message: idInvalid });
+      return;
+    }
+
+    // validate product_id
+    const productIdValid = await db.product.findByPk(product_id);
+    if (!productIdValid) {
+      res.status(500).json({ message: "Invalid product_id!" });
+      return;
+    }
+
+    const descriptionTrimmed = description.trim();
+
+    // check if the user has already created one ???
+    // const reviewExists = await db.review.findAll({
+    //   where: {
+    //     user_id: user_id,
+    //     product_id: product_id,
+    //   },
+    // });
+
+    if (
+      !descriptionTrimmed ||
+      !validate.isLength(descriptionTrimmed, { min: 5, max: 100 })
+    ) {
+      res.status(500).json({ message: "Invalid product review description!" });
+      return;
+    }
+
+    if (stars < 1 || stars > 5) {
+      res.status(500).json({ message: `Invalid star rating! stars, ${stars}` });
+      return;
+    }
+
+    await db.review.create({
+      user_id: user_id,
+      product_id: product_id,
+      description: descriptionTrimmed,
+      stars: stars,
+    });
+
+    res.status(200).json({
+      message: `Success. user_id: ${user_id} has reviewed product_id: ${product_id}`,
+    });
   } catch (e) {
     console.log(e);
     res.status(500).json({ error: "Internal server error" });
