601c0e1e03ff5d22ee123ae61ddf7fd6ad24f0b8
Russell <russells0014@gmail.com>
2024-05-31 14:55:19 +1000

    Update Reviews component to bootstrap card

--- client/src/components/Reviews.tsx
+++ client/src/components/Reviews.tsx
@@ -1,6 +1,8 @@
 import StarRating from "./StarRating";
 import { getProductReviews } from "../service/review";
 import { useEffect, useState } from "react";
+import { AuthConsumer } from "./AuthContext";
+import { Card, Button } from "react-bootstrap";
 
 interface User {
   username: string;
@@ -21,7 +23,26 @@ interface ReviewsProps {
   productId: number;
 }
 
+const trash = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    height={24}
+    width={24}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+    />
+  </svg>
+);
+
 function Reviews({ productId }: ReviewsProps) {
+  const { user } = AuthConsumer();
   const [userReviews, setUserReviews] = useState<Review[]>([]);
 
   useEffect(() => {
@@ -32,23 +53,37 @@ function Reviews({ productId }: ReviewsProps) {
 
   function ReviewCards({ review }: { review: Review }) {
     return (
-      <div className="rounded-1" style={{ backgroundColor: "#cde9d7" }}>
-        <StarRating rating={review.stars} />
-        <div className="d-flex justify-content-between">
-          <div>
-            <h5>{review.title}</h5>
-            <p>{review.description}</p>
-            <p>
-              {review.User.username},{" "}
-              {new Date(review.review_created).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
-            </p>
+      <Card
+        className="rounded-1 border-0 mb-4"
+        style={{ backgroundColor: "#cde9d7" }}
+      >
+        <Card.Body>
+          <StarRating rating={review.stars} />
+          <div className="d-flex justify-content-between align-items-start flex-column">
+            <div>
+              <Card.Title className="pt-1">
+                <h4>{review.title}</h4>
+              </Card.Title>
+              <Card.Text>
+                <p>{review.description}</p>
+              </Card.Text>
+              <Card.Text>
+                {review.User.username},{" "}
+                {new Date(review.review_created).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </Card.Text>
+            </div>
+            {user?.user_id === review.user_id && (
+              <Button variant="" className="p-0 align-self-end">
+                {trash}
+              </Button>
+            )}
           </div>
-        </div>
-      </div>
+        </Card.Body>
+      </Card>
     );
   }
 
