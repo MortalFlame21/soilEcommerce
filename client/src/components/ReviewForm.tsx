593218d4010283cb0ad4212363d799f9f2d6e644
Russell <russells0014@gmail.com>
2024-05-31 23:18:46 +1000

    stars

--- client/src/components/ReviewForm.tsx
+++ client/src/components/ReviewForm.tsx
@@ -12,6 +12,40 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import StarRating from "./StarRating";
 
+const outline = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    width="22"
+    height="22"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+    />
+  </svg>
+);
+
+const solid = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    width="22"
+    height="22"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
 interface ReviewsFormProps {
   productId: number;
   handleUpdate: () => void;
@@ -107,6 +141,8 @@ export default function ReviewsForm({
       values[i] || values[i] == "" ? values[i] : userReview[i]);
   };
 
+  const [rating, setRating] = useState(0);
+
   return (
     <>
       <Row className="mb-4">
@@ -137,42 +173,17 @@ export default function ReviewsForm({
           className="p-5 custom-bg-secondary rounded-3 mb-4"
         >
           <Form.Group className="mb-3">
-            <Form.Label>Stars</Form.Label>
-            <Form.Check
-              type="radio"
-              label="1"
-              value="1"
-              name="stars"
-              onChange={handleChangeValues}
-            />
-            <Form.Check
-              type="radio"
-              label="2"
-              value="2"
-              name="stars"
-              onChange={handleChangeValues}
-            />
-            <Form.Check
-              type="radio"
-              label="3"
-              value={3}
-              name="stars"
-              onChange={handleChangeValues}
-            />
-            <Form.Check
-              type="radio"
-              label="4"
-              value={4}
-              name="stars"
-              onChange={handleChangeValues}
-            />
-            <Form.Check
-              type="radio"
-              label="5"
-              value={5}
-              name="stars"
-              onChange={handleChangeValues}
-            />
+            <Form.Label className="p-0 m-0">Stars</Form.Label>
+            {[1, 2, 3, 4, 5].map((value) => (
+              <Button
+                key={value}
+                onClick={() => setRating(value)}
+                aria-label={`Rate ${value} out of 5`}
+                variant=""
+              >
+                {value <= rating ? solid : outline}
+              </Button>
+            ))}
             <Form.Text className="text-danger">{errors.stars || ""}</Form.Text>
           </Form.Group>
 
