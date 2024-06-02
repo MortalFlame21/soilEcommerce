a7ba23507cb3b508dcc7c5cc64cd67b045c700e9
Russell <russells0014@gmail.com>
2024-06-02 17:53:47 +1000

    GraphQLWsLink

--- admin/src/apollo/client.ts
+++ admin/src/apollo/client.ts
@@ -1,15 +1,14 @@
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { WebSocketLink } from "@apollo/client/link/ws";
 
 const GRAPHQL_ENDPOINT = "ws://localhost:4000/graphql";
 
-const link = new WebSocketLink({
-  uri: GRAPHQL_ENDPOINT,
-  options: {
-    reconnect: true,
-  },
-  webSocketImpl: WebSocket, // Use native WebSocket
-});
+const link = new GraphQLWsLink(
+  createClient({
+    url: GRAPHQL_ENDPOINT,
+  })
+);
 
 const cache = new InMemoryCache();
 
